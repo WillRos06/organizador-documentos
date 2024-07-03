a0aac45d7c7862c3ab8ba941cf23b79a7925a4bb
William Del Rosario <82073842+WillRos06@users.noreply.github.com>
2024-07-03 10:38:55 -0400

    This commit add the program

---
+++ app.py
@@ -0,0 +1,19 @@
+import os
+import shutil
+
+# Ruta del directorio donde están los archivos
+ruta_origen = 'C:/path'
+
+# Lista de extensiones de archivo para clasificar
+extensiones = ['.jpg', '.png', '.pdf', '.txt', '.docx', '.pptx', '.jpeg', '.exe', '.JPG']
+
+for ext in extensiones:
+    # Crea una carpeta para cada tipo de archivo si no existe
+    ruta_destino = os.path.join(ruta_origen, ext[1:].upper())
+    if not os.path.exists(ruta_destino):
+        os.makedirs(ruta_destino)
+
+    # Mueve los archivos al directorio correspondiente
+    for archivo in os.listdir(ruta_origen):
+        if archivo.endswith(ext):
+            shutil.move(os.path.join(ruta_origen, archivo), os.path.join(ruta_destino, archivo))
